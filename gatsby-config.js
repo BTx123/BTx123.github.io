c1d5bcd5542fbeedb81739bd94bb3be4cbc08806
Brian Tom <btom.0831@gmail.com>
2020-09-09 15:15:32 -0700

    Add background and theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,8 +58,8 @@ module.exports = {
         name: `Brian Tom`,
         short_name: `BT`,
         start_url: `/`,
-        // background_color: `#663399`,
-        // theme_color: `#663399`,
+        background_color: `#FFFFFF`,
+        theme_color: `#263238`,
         display: `standalone`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
