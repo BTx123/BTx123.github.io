8f85c1d835839d1cbdcced1013dc6009a27a5049
Brian Tom <btom.0831@gmail.com>
2020-12-10 04:21:52 -0800

    Update resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -9,30 +9,7 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Link from "../components/link";
 
-const ResumePage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      image: file(
-        sourceInstanceName: { eq: "images" }
-        relativePath: { eq: "Brian Tom 20200916.png" }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 1000) {
-            ...GatsbyImageSharpFluid
-            ...GatsbyImageSharpFluidLimitPresentationSize
-          }
-        }
-      }
-      pdf: file(
-        sourceInstanceName: { eq: "data" }
-        relativePath: { eq: "Brian Tom 20200916.pdf" }
-      ) {
-        name
-        publicURL
-      }
-    }
-  `);
-
+const ResumePage = ({ data }) => {
   return (
     <Layout>
       <SEO title="Resume" />
@@ -51,5 +28,28 @@ const ResumePage = () => {
     </Layout>
   );
 };
+
+export const pageQuery = graphql`
+  query {
+    image: file(
+      sourceInstanceName: { eq: "images" }
+      relativePath: { eq: "Brian Tom 20201109.png" }
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+          ...GatsbyImageSharpFluidLimitPresentationSize
+        }
+      }
+    }
+    pdf: file(
+      sourceInstanceName: { eq: "data" }
+      relativePath: { eq: "Brian Tom 20201109.pdf" }
+    ) {
+      name
+      publicURL
+    }
+  }
+`;
 
 export default ResumePage;
